f86abed6f670266161fe6da526b8988681c72103
R. Richards <rdrrichards@hotmail.com>
2019-03-28 19:01:07 -0500

    initial check; basic JWT setup

---
+++ Auth.Api/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Auth.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    // [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("createtoken")]
+        public async Task<IActionResult> CreateToken([FromBody] AuthViewModel authViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await HttpContext.AuthenticateAsync(JwtBearerDefaults.AuthenticationScheme);
+                // var result = await _signinManager.CheckPasswordSignInAsync(user, authViewModel.Password, false);
+                if (user.Succeeded)
+                {
+                    // get the informaton need from the cookie from the SSO server and build out what we need
+                    var claims = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, authViewModel.UserName),
+                        // new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.UniqueName, authViewModel.UserName),
+                        // new Claim("PrimaryAccount", user.PrimaryAccount.AccountCode)
+                        new Claim("user_roles", "user,account_admin")
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+                    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken(_configuration["Tokens:Issuer"], _configuration["Tokens:Audience"], claims,
+                        expires: DateTime.UtcNow.AddMinutes(60), signingCredentials: creds);
+
+                    var results = new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expires = token.ValidTo
+                    };
+
+                    return Created("", results);
+                }
+            }
+            return BadRequest();
+        }
+    }
+    public class AuthViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Auth.Api/Startup.cs
+++ Auth.Api/Startup.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Auth.Api
 {
@@ -25,7 +27,44 @@ namespace Auth.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "QIQO Authentication API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AnyOrigin", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(options => {
+                options.DefaultScheme = "MainCookie";
+            });
+            services.AddAuthentication(options => {
+                options.DefaultScheme = "ExternalCookie";
+            });
+            services.AddAuthentication().AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = Configuration["Tokens:Issuer"],
+                    ValidAudience = Configuration["Tokens:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"]))
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +79,14 @@ namespace Auth.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "QIQO Authentication API V1");
+            });
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseCors("AnyOrigin");
 
             app.UseHttpsRedirection();
             app.UseMvc();
